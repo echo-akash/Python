ff1e2a9f02c4ca694f1c8dd75fb7f1573dd66e86
akash poddar <akash.poddar.0799@gmail.com>
2019-10-02 21:24:07 +0600

    Add files via upload

---
+++ PythonDependencySetup.bat
@@ -0,0 +1,244 @@
+rem cmd to install python dependencies
+rem description of each dependency written in line before the cmd
+rem ensure python and pip is installed before working with it
+rem use pip3 if you are using pip3 version instead of pip (select and replace all pip by pip3)
+rem This repository is a collection of Python library code for building Python applications. The code is collected from Google's own Python code base, and has been extensively tested and used in production.
+pip install absl-py
+rem A configurable sidebar-enabled Sphinx theme
+pip install alabaster
+rem Read/rewrite/write Python ASTs
+pip install astor
+rem Classes Without Boilerplate
+pip install attrs
+rem A collection of tools for internationalizing Python applications.
+pip install Babel
+rem Specifications for callback functions passed in to an API
+pip install backcall
+rem The weakref module allows the Python programmer to create weak references to objects.
+pip install backports.weakref
+rem Bleach is an allowed-list-based HTML sanitizing library that escapes or strips markup and attributes.
+pip install bleach
+rem Certifi is a carefully curated collection of Root Certificates for validating the trustworthiness of SSL certificates while verifying the identity of TLS hosts. It has been extracted from the Requests project.
+pip install certifi
+rem Chardet: The Universal Character Encoding Detector
+pip install chardet
+rem Makes ANSI escape character sequences (for producing colored terminal text and cursor positioning) work under MS Windows.
+pip install colorama
+rem This module provides utilities for common tasks involving the with statement.
+pip install contextlib2
+rem The public API of cycler consists of a class Cycler, a factory function cycler(), and a concatenation function concat(). The factory function provides a simple interface for creating ‘base’ Cycler objects while the class takes care of the composition and iteration logic.
+pip install Cycler
+rem The Cython language makes writing C extensions for the Python language as easy as Python itself.
+pip install Cython
+rem The goal of the decorator module is to make it easy to define signature-preserving function decorators and decorator factories.
+pip install decorator
+rem XML bomb protection for Python stdlib modules
+pip install defusedxml
+rem Docutils is a modular system for processing documentation into useful formats, such as HTML, XML, and LaTeX.
+pip install docutils
+rem Entry points are a way for Python packages to advertise objects with some common interface.
+pip install entrypoints
+rem a installable Python package so users can do easy_install ez_setup troublesome_package as a workaround.
+pip install ez_setup
+rem GAST provides a compatibility layer between the AST(Abstract Syntax Tree) of various Python versions, as produced by ast.parse from the standard ast module.
+pip install gast
+rem HTTP/2-based RPC framework
+pip install grpcio
+rem Read and write HDF5 files from Python
+pip install h5py
+rem Internationalized Domain Names in Applications (IDNA)
+pip install idna
+rem Library for reading and writing a wide range of image, video, scientific, and volumetric data formats.
+pip install imageio
+rem Getting image size from png/jpeg/jpeg2000/gif file
+pip install imagesize
+rem IPython: Productive Interactive Computing
+pip install ipython
+rem IPython Kernel for Jupyter
+pip install ipykernel
+rem Interactive Parallel Computing with IPython
+pip install ipyparallel
+rem Vestigial utilities from IPython
+pip install ipython_genutils
+rem IPython HTML widgets for Jupyter
+pip install ipywidgets
+rem An autocompletion tool for Python that can be used for text editors.
+pip install jedi
+rem A small but fast and easy to use stand-alone template engine written in pure python.
+pip install Jinja2
+rem Lightweight pipelining: using Python functions as pipeline jobs.
+pip install joblib
+rem An implementation of JSON Schema validation for Python
+pip install jsonschema
+rem Jupyter metapackage. Install all the Jupyter components in one go.
+pip install jupyter
+rem Jupyter protocol implementation and client libraries
+pip install jupyter-client
+rem Jupyter terminal console
+pip install jupyter_console
+rem Jupyter core package. A base package on which Jupyter projects rely.
+pip install jupyter-core
+rem Deep Learning for humans
+pip install Keras
+rem Reference implementations of popular deep learning models
+pip install Keras-Applications
+rem Easy data preprocessing and data augmentation for deep learning models
+pip install Keras-Preprocessing
+rem A fast implementation of the Cassowary constraint solver
+pip install kiwisolver
+rem LabelImg is a graphical image annotation tool and label object bounding boxes in images
+pip install labelImg
+rem Powerful and Pythonic XML processing library combining libxml2/libxslt with the ElementTree API.
+pip install lxml
+rem Markdown is a text-to-HTML conversion tool for web writers.
+pip install Markdown
+rem Safely add untrusted strings to HTML/XML markup.
+pip install MarkupSafe
+rem Python plotting package
+pip install matplotlib
+rem The fastest markdown parser in pure Python
+pip install mistune
+rem mock is a library for testing in Python. It allows you to replace parts of your system under test with mock objects and make assertions about how they have been used.
+pip install mock
+rem Converting Jupyter Notebooks
+pip install nbconvert
+rem This package contains the base implementation of the Jupyter Notebook format, and Python APIs for working with notebooks.
+pip install nbformat
+rem Python package for creating and manipulating graphs and networks
+pip install networkx
+rem nose extends the test loading and running features of unittest, making
+pip install nose
+rem A web-based notebook environment for interactive computing
+pip install notebook
+rem NumPy is the fundamental package for array computing with Python.
+pip install numpy
+rem A package build from Tensorflow's object detection API.
+pip install object-detection
+rem Wrapper package for OpenCV python bindings
+pip install opencv-python
+rem Core utilities for Python packages
+pip install packaging
+rem Powerful data structures for data analysis, time series, and statistics
+pip install pandas
+rem Utilities for writing pandoc filters in python
+pip install pandocfilters
+rem Parso is a Python parser that supports error recovery and round-trip parsing for different Python versions (in multiple Python versions). Parso is also able to list multiple syntax errors in your python file.
+pip install parso
+rem Tiny 'shelve'-like database with concurrency support
+pip install pickleshare
+rem Python Imaging Library (Fork)
+pip install Pillow
+rem Query metadatdata from sdists / bdists / installed packages.
+pip install pkginfo
+rem Python client for the Prometheus monitoring system.
+pip install prometheus_client
+rem Library for building powerful interactive command lines in Python
+pip install prompt_toolkit
+rem Protocol Buffers are Google’s data interchange format
+pip install protobuf
+rem pycocotools
+rem Install Visual C++ 2015 Build Tools from https://go.microsoft.com/fwlink/?LinkId=691126 with default selection.
+rem Go to C:\Program Files (x86)\Microsoft Visual C++ Build Tools and run vcbuildtools_msbuild.bat
+rem In Anaconda, run pip install git+https://github.com/philferriere/cocoapi.git#egg=pycocotools^&subdirectory=PythonAPI
+rem Pygments is a syntax highlighting package written in Python.
+pip install Pygments
+rem Python parsing module
+pip install pyparsing
+rem Python bindings for the Qt cross platform UI and application toolkit
+pip install PyQt5
+rem Python extension module support for PyQt5
+pip install PyQt5-sip
+rem Pyrsistent is a number of persistent collections (by some referred to as functional data structures). Persistent in the sense that they are immutable.
+pip install pyrsistent
+rem Extensions to the standard Python datetime module
+pip install python-dateutil
+rem World timezone definitions, modern and historical
+pip install pytz
+rem PyWavelets, wavelet transform module
+pip install PyWavelets
+rem Python bindings for the winpty library
+pip install pywinpty
+rem YAML is a data serialization format designed for human readability and interaction with scripting languages. PyYAML is a YAML parser and emitter for Python.
+pip install PyYAML
+rem Python bindings for 0MQ
+pip install pyzmq
+rem Jupyter Qt console
+pip install qtconsole
+rem eadme_renderer is a library for rendering "readme" descriptions for Warehouse
+pip install readme_renderer
+rem Python HTTP for Humans.
+pip install requests
+rem A utility belt for advanced users of python-requests
+pip install requests-toolbelt
+rem Image processing routines for SciPy
+pip install scikit-image
+rem A set of python modules for machine learning and data mining
+pip install scikit-learn
+rem An intuitive library to add plotting functionality to scikit-learn objects.
+pip install scikit-plot
+rem SciPy: Scientific Library for Python
+pip install scipy
+rem seaborn: statistical data visualization
+pip install seaborn
+rem Send file to trash natively under Mac OS X, Windows and Linux.
+pip install Send2Trash
+rem Easily download, build, install, upgrade, and uninstall Python packages
+pip install setuptools
+rem Six is a Python 2 and 3 compatibility library. It provides utility functions for smoothing over the differences between the Python versions with the goal of writing Python code that is compatible on both Python versions.
+pip install six
+rem A set of python modules for machine learning and data mining
+pip install scikit-learn
+rem This package provides 26 stemmers for 25 languages generated from Snowball algorithms.
+pip install snowballstemmer
+rem Python documentation generator
+pip install Sphinx
+rem sphinxcontrib-applehelp is a sphinx extension which outputs Apple help books
+pip install sphinxcontrib-applehelp
+rem sphinxcontrib-devhelp is a sphinx extension which outputs Devhelp document.
+pip install sphinxcontrib-devhelp
+rem sphinxcontrib-devhelp is a sphinx extension
+pip install sphinxcontrib-htmlhelp
+rem A sphinx extension which renders display math in HTML via JavaScript
+pip install sphinxcontrib-jsmath
+rem sphinxcontrib-qthelp is a sphinx extension which outputs QtHelp document.
+pip install sphinxcontrib-qthelp
+rem sphinxcontrib-serializinghtml is a sphinx extension which outputs "serialized" HTML files (json and pickle).
+pip install sphinxcontrib-serializinghtml
+rem Because spellchecker. SciPy at your fingertips
+pip install spicy
+rem TensorFlow is an open source machine learning framework for everyone
+pip install tensorflow
+rem TensorBoard is a suite of web applications for inspecting and understanding your TensorFlow runs and graphs.
+pip install tensorboard
+rem TensorFlow Estimator is a high-level API that encapsulates model training, evaluation, prediction, and exporting.
+pip install tensorflow-estimator
+rem Tensorflow Object Detection Library Packaged
+pip install tensorflow-object-detection-api
+rem ANSII Color formatting for output in terminal.
+pip install termcolor
+rem Terminals served to xterm.js using Tornado websockets
+pip install terminado
+rem Testpath is a collection of utilities for Python code working with files and commands.
+pip install testpath
+rem Tornado is a Python web framework and asynchronous networking library, originally developed at FriendFeed.
+pip install tornado
+rem Fast, Extensible Progress Meter
+pip install tqdm
+rem A configuration system for Python applications.
+pip install traitlets
+rem Collection of utilities for publishing packages on PyPI
+pip install twine
+rem HTTP library with thread-safe connection pooling, file post, and more
+pip install urllib3
+rem Measures number of Terminal column cells of wide-character codes
+pip install wcwidth
+rem Character encoding aliases for legacy web content
+pip install webencodings
+rem The comprehensive WSGI web application library.
+pip install Werkzeug
+rem This library is the reference implementation of the Python wheel packaging standard
+pip install wheel
+rem IPython HTML widgets for Jupyter
+pip install widgetsnbextension
+rem XGBoost Python Package
+pip install xgboost
