e74e4672a039198c388687ae2e2093cdccd9a29f
akash poddar <akash.poddar.0799@gmail.com>
2019-12-07 20:10:03 +0600

    Update PythonDependencySetup.bat

--- PythonDependencySetup.bat
+++ PythonDependencySetup.bat
@@ -248,3 +248,5 @@ rem Alternative regular expression module, to replace re.
 pip install regex
 rem A little word cloud generator in Python
 pip install wordcloud
+rem Geographic pandas extensions
+pip install geopandas
